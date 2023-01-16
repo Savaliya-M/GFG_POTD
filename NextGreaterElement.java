22ec5300b07eea8fbb2bb45c0fa7c0f67f245ec3
Savaliya-M <masavaliya183@gmail.com>
2023-01-16 16:23:40 +0530

    Day 1

--- NextGreaterElement.java
+++ NextGreaterElement.java
@@ -18,6 +18,7 @@ public class NextGreaterElement {
             for (int i = 0; i < n; i++)
                 System.out.print(res[i] + " ");
             System.out.println();
+
         }
     }
 }
